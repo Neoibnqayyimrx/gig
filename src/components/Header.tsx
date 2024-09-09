8647a04fe051d2d6c58c1410e9c90d0b88148ae8
Neoibnqayyimrx <neoibnqayyim@hotmail.com>
2024-09-09 22:36:31 +0100

    added new components

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const Header = () => {
+  return (
+    <div className='bg-bodyColor h-20'>
+      <Container>
+        <h2>Logo</h2>
+      </Container>
+    </div>
+  )
+}
+
+export default Header
