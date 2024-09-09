a0573c66e7b4751c135d521a1527f10ea0ac93e1
Neoibnqayyimrx <neoibnqayyim@hotmail.com>
2024-09-09 22:39:02 +0100

    updated container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,7 +5,7 @@
 
 const Container = ({ children,className} : Props) => {
   return (
-    <div className={`${className} max-w-screen-xl mx-auto px-4 xl:px-0`}>
+    <div className={`${className} max-w-screen-xl mx-auto px-4 xl:px-0 py-10`}>
         {children}
     </div>
   )
